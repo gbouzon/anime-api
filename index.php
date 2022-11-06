a5b13890e00fc9148b5a9639304af2175aeef7e7
Giuliana Bouzon <ga@bouzon.com.br>
2022-11-06 14:44:39 -0500

    fixed index & GET routes added

--- index.php
+++ index.php
@@ -12,33 +12,54 @@ require_once './includes/helpers/helper_functions.php';
 
 //routes
 require_once './includes/routes/base_routes.php';
-require_once './includes/routes/artists_routes.php';
-require_once './includes/routes/customers_routes.php';
-require_once './includes/routes/album_routes.php';
+require_once './includes/routes/anime_routes.php';
+require_once './includes/routes/manga_routes.php';
+require_once './includes/routes/review_routes.php';
+require_once './includes/routes/studio_routes.php';
+require_once './includes/routes/user_routes.php';
+require_once './includes/routes/genre_routes.php';
 
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-$app->setBasePath("/webservices/music-api");
+$app->setBasePath("/webservices/anime-api");
 
 //Root
 $app->get('/', "getResources"); 
 
 //Data retrieval - HTTP GET methods for the exposed resources
-$app->get('/artists', "getAllArtists");
-$app->get('/artists/{artist_id:[0-9]+}', "getArtistById"); 
-$app->get('/artists/{artist_id:[0-9]+}/albums', "getAlbumsByArtistId"); 
-$app->get('/artists/{artist_id:[0-9]+}/albums/{album_id:[0-9]+}/tracks', "getTracksByArtistAlbum"); 
-$app->get('/customers/{customer_id:[0-9]+}/invoices', "getPurchasesByCustomerId");
-$app->get('/invoices', "getPurchasesByCustomerId");
-$app->get('/customers', "getAllCustomers"); 
+$app->get('/anime', "getAllAnime");
+$app->get('/anime/{anime_id:[0-9]+}', "getAnimeById");
+$app->get('/anime/{anime_id:[0-9]+}/reviews', "getAnimeReviews");
+
+
+$app->get('/manga', "getAllManga");
+$app->get('/manga/{manga_id:[0-9]+}', "getMangaById");
+$app->get('/manga/{manga_id:[0-9]+}/reviews', "getMangaReviews");
+
+$app->get('/users', "getAllUsers");
+$app->get('/users/{user_id:[0-9]+}', "getUserById");
+$app->get('/users/{user_id:[0-9]+}/reviews', "getUserReviews");
+$app->get('/users/{user_id:[0-9]+}/toWatch/anime', "getUserAnimeToWatch");
+$app->get('/users/{user_id:[0-9]+}/watched/anime', "getUserAnimeWatched");
+$app->get('/users/{user_id:[0-9]+}/toWatch/manga', "getUserMangaToWatch");
+$app->get('/users/{user_id:[0-9]+}/watched/manga', "getUserMangaWatched");
+
+$app->get('/studios', "getAllStudios");
+$app->get('/studios/{studio_id:[0-9]+}', "getStudioById");
+$app->get('/studios/{studio_id:[0-9]+}/anime', "getStudioAnime");
+
+$app->get('/reviews', "getAllReviews");
+$app->get('/reviews/{review_id:[0-9]+}', "getReviewById");
+
+$app->get('/genres', "getAllGenres");
+$app->get('/genres/{genre_id:[0-9]+}', "getGenreById");
+$app->get('/genres/{genre_id:[0-9]+}/anime', "getGenreAnime"); //Giu Build 1
+$app->get('/genres/{genre_id:[0-9]+}/manga', "getGenreManga"); //Giu Build 1
 
 //Data manipulation - HTTP POST, PUT, and DELETE methods for the exposed resources
-$app->post('/artists', "createArtists"); 
-$app->put('/artists', "updateArtists"); 
-$app->delete('/artists/{artist_id:[0-9]+}', "deleteArtist"); 
-$app->delete('/customers/{customer_id:[0-9]+}', "deleteCustomer"); 
+
 
 // Run the app.
 $app->run();
