f45dd4e41d6bd80b6034d9c0540b1a6af851137a
danichh <70587707+danichh@users.noreply.github.com>
2022-11-13 01:46:17 -0500

    Implement GET methods for  User watch and to-watched lists(#19, #18. #17 #16 )

--- includes/routes/user_routes.php
+++ includes/routes/user_routes.php
@@ -9,3 +9,72 @@ require_once __DIR__ . './../routes/base_routes.php';
 require_once __DIR__ . './../models/BaseModel.php';
 require_once __DIR__ . './../models/UserModel.php';
 
+
+function getUserMangaWatched(Request $request, Response $response, array $args) {
+    $manga_info = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $user_model = new UserModel();
+
+    // Retrieve the user if from the request's URI.
+    $user_id= $args["user_id"];
+    if (isset($user_id)) {
+        // Fetch the info about the specified user.
+        $manga_info = $user_model->getUserMangaWatched($user_id);
+        return checkData($manga_info, $response, $request);
+    }
+    return unsupportedOperation($request, $response); 
+}
+
+
+function getUserMangaToWatch(Request $request, Response $response, array $args) {
+    $manga_info = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $user_model = new UserModel();
+
+    // Retrieve the user if from the request's URI.
+    $user_id= $args["user_id"];
+    if (isset($user_id)) {
+        // Fetch the info about the specified user.
+        $manga_info = $user_model->getUserMangaToWatch($user_id);
+        return checkData($manga_info, $response, $request);
+    }
+    return unsupportedOperation($request, $response); 
+}
+
+function getUserAnimeWatched(Request $request, Response $response, array $args) {
+    $anime_info = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $user_model = new UserModel();
+
+    // Retrieve the user if from the request's URI.
+    $user_id= $args["user_id"];
+    if (isset($user_id)) {
+        // Fetch the info about the specified user.
+        $anime_info = $user_model->getUserAnimeWatched($user_id);
+        return checkData($anime_info, $response, $request);
+    }
+    return unsupportedOperation($request, $response); 
+}
+
+function getUserAnimeToWatch(Request $request, Response $response, array $args) {
+    $anime_info = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $user_model = new UserModel();
+
+    // Retrieve the user if from the request's URI.
+    $user_id= $args["user_id"];
+    if (isset($user_id)) {
+        // Fetch the info about the specified user.
+        $anime_info = $user_model->getUserAnimeToWatch($user_id);
+        return checkData($anime_info, $response, $request);
+    }
+    return unsupportedOperation($request, $response); 
+}
+
+ 
+
+
